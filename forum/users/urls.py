012d48031ced43a7a1c1025c2c290b3b1b8f1bbc
Dmitry G <gupalo1108@gmail.com>
2018-01-31 13:55:11 +0300

    Initial django-cookiecutter 2.0

---
+++ forum/users/urls.py
@@ -0,0 +1,28 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path(
+        '',
+        view=views.UserListView.as_view(),
+        name='list'
+    ),
+    path(
+        '~redirect/',
+        view=views.UserRedirectView.as_view(),
+        name='redirect'
+    ),
+    path(
+        '<username>/',
+        view=views.UserDetailView.as_view(),
+        name='detail'
+    ),
+    path(
+        '~update/',
+        view=views.UserUpdateView.as_view(),
+        name='update'
+    ),
+]
+
+# [\w.@+-]+
